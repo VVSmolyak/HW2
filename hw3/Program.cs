d4b4f092ae7a4b726c8189d33c2017f2b290082c
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2022-10-28 23:05:58 +0300

    hw3 added

---
+++ hw3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine ("Введите цифру: ");
+int number = int.Parse (Console.ReadLine ()!);
+
+if (checkweekends (number)) {
+    Console.WriteLine ("да");
+}
+else {
+    Console.WriteLine ("нет");
+}
+
+bool checkweekends (int number)
+{
+    return number == 6 || number == 7;
+}
